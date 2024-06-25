f19e8dd49cd19e4011ff833c7a93693d745fec34
Akbar Hafeez <akbarhafeez25@gmail.com>
2024-06-25 21:08:38 +0500

    config updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "images.pexels.com",
-//       },
-//     ],
-//   },
-// };
-
-// export default nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -21,7 +8,6 @@ const nextConfig = {
       },
     ],
   },
-  // Add the static export configuration
-  output: 'export', // This enables the static export
 };
+
 export default nextConfig;
